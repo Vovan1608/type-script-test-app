dcbb5cd061f0dac9c92e011233f8095503373304
Volodymyr Zenin <vzeni@softserveinc.com>
2022-06-09 17:10:48 +0300

    add ampersand

--- lessons/app_4Alias.js
+++ lessons/app_4Alias.js
@@ -3,5 +3,6 @@ function fetchWithAuthAlias(url, method) { }
 const user = {
     name: 'Bob',
     age: 32,
-    skills: ['Dev', 'DevOps']
+    skills: ['Dev', 'DevOps'],
+    id: 85022210002155
 };
--- lessons/app_4Alias.ts
+++ lessons/app_4Alias.ts
@@ -3,13 +3,20 @@ type httpMethod = 'get' | 'put';
 function fetchWithAuthAlias(url: string, method: httpMethod) {}
 
 type User = {
-    name: string,
-    age: number,
-    skills: string[]
+    name: string;
+    age: number;
+    skills: string[];
 };
 
-const user: User = {
+type Role = {
+    id: number;
+}
+
+type UserWithRole = User & Role;
+
+const user: UserWithRole = {
     name: 'Bob',
     age: 32,
-    skills: ['Dev', 'DevOps']
-}
+    skills: ['Dev', 'DevOps'],
+    id: 85022210002155
+};
