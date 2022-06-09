b81786182609c613b0089ca4f6c04c117a31c170
Volodymyr Zenin <vzeni@softserveinc.com>
2022-06-09 17:50:20 +0300

    add examples

---
+++ lessons/app_6TypeVsInterface.js
@@ -0,0 +1,7 @@
+"use strict";
+;
+;
+const chel = {
+    name: 'Bob',
+    age: 55
+};
--- lessons/app_6TypeVsInterface.ts
+++ lessons/app_6TypeVsInterface.ts
@@ -10,3 +10,9 @@ const chel: Chel = {
     name: 'Bob',
     age: 55
 };
+
+type Id = string | number;
+
+interface ID {
+    id: string | number
+}
