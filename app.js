484de672489b6e5adfbb6375d4dec760bf4eb8fa
Volodymyr Zenin <vzeni@softserveinc.com>
2022-06-09 13:32:33 +0300

    add tuples, readonly

--- app.js
+++ app.js
@@ -1,12 +1,37 @@
 "use strict";
-const getFullName = (userEntity) => {
-    return `${userEntity.firstName} ${userEntity.lastName}`;
-};
-const user = {
-    firstName: 'Bob',
-    lastName: 'Diul',
-    age: 25,
-    city: 'N'
-};
-const res = getFullName(user);
-console.log(res);
+// FUNCTION
+// const getFullName = (userEntity: { firstName: string, lastName: string}): string => {
+//     return `${userEntity.firstName} ${userEntity.lastName}`;
+// };
+// const user = {
+//     firstName: 'Bob',
+//     lastName: 'Diul',
+//     age: 25,
+//     city: 'N'
+// };
+// const res = getFullName(user);
+// console.log(res);
+// FIXME: ARRAY
+// const skills: string[] = ['dev', 'devOps', 'testing'];
+// for(const skill of skills) {
+//     console.log(skill.toUpperCase());  
+// }
+// const newArr = skills
+//                 .filter(skill => skill !== 'devOps')
+//                 .map(skill => 1)
+//                 .reduce((cur, next) => cur + next);
+// console.log(newArr);
+// TODO: TUPLES
+// const skills: [number, string] = [1, 'devOps'];
+// const id = skills[0];
+// const skillName = skills[1];
+// skills.push(2);
+// // skills.push(false);
+// // const some = skills[2];
+// const newSkill: [number, string, ...boolean[]] = [1, 'dev'];
+// TODO: READONLY
+// const someArr: readonly [number, string] = [1, 'dev'];
+// // someArr[0] = 5;
+// const someElseArr: ReadonlyArray<string> = ['Dev', 'DevOps'];
+// someElseArr.push('njutd');
+// TODO: ENUMS
--- tasks/task_1.js
+++ tasks/task_1.js
@@ -1,2 +1,12 @@
 "use strict";
-let info;
+let info = {
+    officeId: 44,
+    isOpened: false,
+    contacts: {
+        phone: '09356789412',
+        email: 'email@mail.com',
+        address: {
+            city: 'Dnipro'
+        }
+    }
+};
