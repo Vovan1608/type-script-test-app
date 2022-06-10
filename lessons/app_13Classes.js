4bcefd408aa0f2cb86c208a6d348b46323de4f55
Volodymyr Zenin <vzeni@softserveinc.com>
2022-06-10 17:06:57 +0300

    add overload examples

--- lessons/app_13Classes.js
+++ lessons/app_13Classes.js
@@ -1,11 +1,23 @@
 "use strict";
 class MyUser {
-    constructor(name) {
-        this.name = name;
+    constructor(nameOrAge, age) {
+        if (typeof nameOrAge === 'string') {
+            this.name = nameOrAge;
+        }
+        else if (typeof nameOrAge === 'number') {
+            this.age = nameOrAge;
+        }
+        if (typeof age === 'number') {
+            this.age = age;
+        }
     }
 }
 const myUser = new MyUser('Don');
-class MyAdmin {
-}
-const myAdmin = new MyAdmin();
-myAdmin.role = 1;
+const myUser2 = new MyUser();
+const myUser3 = new MyUser(33);
+const myUser4 = new MyUser('Tom', 45);
+// class MyAdmin {
+//     role: number;
+// }
+// const myAdmin = new MyAdmin();
+// myAdmin.role = 1;
--- lessons/app_13Classes.ts
+++ lessons/app_13Classes.ts
@@ -1,16 +1,32 @@
 class MyUser {
     name: string;
+    age: number;
 
-    constructor(name: string) {
-        this.name = name;
+    constructor();
+    constructor(name: string);
+    constructor(age: number);
+    constructor(name:string, age: number);
+    constructor(nameOrAge?: string | number, age?: number) {
+        if (typeof nameOrAge === 'string') {
+            this.name = nameOrAge;
+        } else if (typeof nameOrAge === 'number') {
+            this.age = nameOrAge;
+        }
+
+        if (typeof age === 'number') {
+            this.age = age
+        }
     }
 }
 
 const myUser = new MyUser('Don');
+const myUser2 = new MyUser();
+const myUser3 = new MyUser(33);
+const myUser4 = new MyUser('Tom', 45);
 
-class MyAdmin {
-    role: number;
-}
+// class MyAdmin {
+//     role: number;
+// }
 
-const myAdmin = new MyAdmin();
-myAdmin.role = 1;
+// const myAdmin = new MyAdmin();
+// myAdmin.role = 1;
