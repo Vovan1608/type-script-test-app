26d788771c13f8286ad69ed4406d8944103d35a3
Volodymyr Zenin <vzeni@softserveinc.com>
2022-06-10 14:04:42 +0300

    add null examples

---
+++ lessons/app_10Null.js
@@ -0,0 +1,7 @@
+"use strict";
+const n = null;
+const n1 = null;
+// const n2: number = null;
+// const n3: string = null;
+// const n4: boolean = null;
+// const n5: undefined = null;
---
+++ lessons/app_10Null.ts
@@ -0,0 +1,6 @@
+const n: null = null;
+const n1: any = null;
+// const n2: number = null;
+// const n3: string = null;
+// const n4: boolean = null;
+// const n5: undefined = null;
